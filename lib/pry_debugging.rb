64720cb62182eaa9b4eeefae23c39f4de08bfa91
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-07 07:16:29 +0300

    commit tree c60f226f7c7adc6450435648950da8121e24d98c

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,8 +1,8 @@
-# $num = 3
+# require 'pry'
 def plus_two(num)
-	# modify function to return 5 directly
-	5
-	# num + 2
+	# # modify function to return 5 directly
+	# 5
+	num + 2
 	# binding.pry
 	# num
 end
